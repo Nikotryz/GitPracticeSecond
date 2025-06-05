31d44285caf4b98603113ad1d15b1ac88179653d
Nikotryz <nikitich900440@gmail.com>
2025-06-06 01:04:25 +0700

    Add a TaskModel class

---
+++ GitPractice/TaskModel.cs
@@ -0,0 +1,37 @@
+﻿namespace GitPractice
+{
+    public class TaskModel
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public DateTime Deadline { get; private set; }
+
+        public TaskModel(string name, string description, string deadline)
+        {
+            Name = name;
+            Description = description;
+            Deadline = DateTime.Parse(deadline);
+        }
+
+        public void SetId(int id)
+        {
+            Id = id;
+        }
+
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+
+        public void SetDescription(string description)
+        {
+            Description = description;
+        }
+
+        public void SetDeadline(string deadline)
+        {
+            Deadline = DateTime.Parse(deadline);
+        }
+    }
+}
