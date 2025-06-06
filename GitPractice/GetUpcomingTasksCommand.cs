afe3c62e12bba995056d9bffc21c3ae2aef7a3c6
Nikotryz <nikitich900440@gmail.com>
2025-06-06 17:56:49 +0700

    Add a get upcoming tasks command and change merge conflict file

---
+++ GitPractice/GetUpcomingTasksCommand.cs
@@ -0,0 +1,21 @@
+﻿namespace GitPractice
+{
+    class GetUpcomingTasksCommand
+    {
+        private readonly TaskHelper _taskHelper = new();
+
+        public void GetUpcomingTasks()
+        {
+            var tasks = _taskHelper.GetAllTasks();
+            var upcomingTasks = tasks?.Where(x => x.Deadline > DateTime.Now);
+            if (upcomingTasks == null)
+            { 
+                return; 
+            }
+            foreach (var task in upcomingTasks)
+            {
+                Console.WriteLine($"Task ID: {task.Id}\nTask name: {task.Name}\nTask description: {task.Description}\nTask deadline: {task.Deadline}");
+            }
+        }
+    }
+}
